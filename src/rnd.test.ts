3048f0ea1b1935d80332fa9bd2c37639e74cba5b
Henrik Collin <henrik.collin@cinia.fi>
2021-10-20 15:48:04 +0300

    rnd() unit tests

--- src/rnd.test.ts
+++ src/rnd.test.ts
@@ -1,17 +1,33 @@
 import { rnd } from "./rnd";
 
 describe("rnd() tests", () => {
-
     const repeats = 100;
 
-    it(`Must accept 2 arguments, run ${repeats} times`, () => {
+    it(`Must accept 2 arguments, executed ${repeats} times`, () => {
+        const results: number[] = [];
+        for (let i = 0; i < repeats; i++) {
+            const res = rnd(1, 10);
+            expect(res).toBeGreaterThanOrEqual(1);
+            expect(res).toBeLessThanOrEqual(10);
+            if(!results[res]) {
+                results[res] = 0;
+            }
+            results[res]++;
+            expect(results[res]).toBeLessThan(repeats*0.75);
+        }
+    });
 
-        for(let i = 0; i < repeats ; i++) {
-            const res = rnd(1,10);
+    it(`Must accept 1 argument, executed ${repeats} times`, () => {
+        const results: number[] = [];
+        for (let i = 0; i < repeats; i++) {
+            const res = rnd(10);
             expect(res).toBeGreaterThanOrEqual(0);
             expect(res).toBeLessThanOrEqual(10);
+            if(!results[res]) {
+                results[res] = 0;
+            }
+            results[res]++;
+            expect(results[res]).toBeLessThan(repeats*0.75);
         }
-
-
     });
-})
+});
